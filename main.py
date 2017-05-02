5e49bfdcf42a03e86607e4542f3273eea2201649
Lapa <thinklnp@yandex.ru>
2017-05-03 01:09:56 +0300

    add not null

--- main.py
+++ main.py
@@ -13,35 +13,38 @@ def create_table(conn, t):
 
 
 def create_db(conn, f):
-    create_table(conn,{"name":"am_meta", "columns":{"met_id":"INT", "dt":"DATETIME2 DEFAULT GETDATE()"}})
+    create_table(conn,{"name":"am_meta", "columns":{"met_id":"INT NOT NULL PRIMARY KEY IDENTITY(1,1)",
+                                                    "dt":"DATETIME2 NOT NULL DEFAULT GETDATE()"}})
     for s_o in f["source_objects"]:
         create_table(conn, {"name": s_o["name"] + "_" + s_o["delta_field"],
-                                    "columns":{s_o["delta_field"]: "BIGINT",
-                                               "met_id": "INT",
-                                               "dt": "DATETIME 2 DEFAULT GETDATE()"}})
+                                    "columns":{s_o["delta_field"]: "BIGINT NOT NULL",
+                                               "met_id": "INT NOT NULL PRIMARY KEY",
+                                               "dt": "DATETIME2 NOT NULL DEFAULT GETDATE()"}})
     for o in f["objects"]:
         if o["type"] == "anchor":
             create_table(conn, {"name": o["name"],
                                 "columns": {
-                                    o["name"] + "_id": o["column_type"],
-                                    "met_id": "INT"
+                                    o["name"] + "_id": o["column_type"] + " NOT NULL PRIMARY KEY",
+                                    "met_id": "INT NOT NULL"
                                 }})
         elif o["type"] == "attribute":
             a = [t_o for t_o in f["objects"] and t_o["type"] == "anchor" and t_o["name"] == o["anchor"]][0]
             create_table(conn, {"name": a["name"]+o["name"],
                                 "columns":{
-                                    a["name"]+"_id": a["column_type"],
-                                    o["column_name"]: o["column_type"],
-                                    "met_id": "INT"
+                                    a["name"]+"_id": a["column_type"] + " NOT NULL PRIMARY KEY REFERENCES "
+                                    + a["name"] + "(" + a["name"] + "_id) ",
+                                    o["column_name"]: o["column_type"] + " NOT NULL",
+                                    "met_id": "INT NOT NULL"
                                 }})
         elif o["type"] == "historical_attribute":
             a = [t_o for t_o in f["objects"] and t_o["type"] == "anchor" and t_o["name"] == o["anchor"]][0]
             create_table(conn, {"name": a["name"] + o["name"],
                                 "columns": {
-                                    a["name"] + "_id": a["column_type"],
-                                    o["column_name"]: o["column_type"],
-                                    "changedDT": "DATETIME2",
-                                    "met_id": "INT"
+                                    a["name"] + "_id": a["column_type"] + " NOT NULL PRIMARY KEY REFERENCES "
+                                    + a["name"] + "(" + a["name"] + "_id) ",
+                                    o["column_name"]: o["column_type"] + " NOt NULL",
+                                    "changedDT": "DATETIME2 NOT NULL DEFAULT GETDATE()",
+                                    "met_id": "INT NOT NULL"
                                 }})
 
 
