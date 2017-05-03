b1d8e68cb93325ca7dfc2c3b9713f980db8d3272
Пекальн Николай Александрович <pekaln@wildberries.ru>
2017-05-03 20:58:54 +0300

    working create

--- main.py
+++ main.py
@@ -3,23 +3,33 @@ import json
 
 
 def create_table(conn, t):
-    if not conn.tables(table=t["name"]).fetchone():
+    crsr = conn["connection"].cursor()
+    if not crsr.tables(table=t["name"], schema=conn["schema"], catalog=conn["database"]).fetchone():
         create_script = "CREATE TABLE "
-        create_script += conn.schema + "." + t["name"] + " ( "
-        create_script += ", ".join([c_n + " " + c_t for c_n, c_t in t["columns"]])
+        create_script += conn["schema"] + "." + t["name"] + " ( "
+        create_script += ", ".join([c_n + " " + c_t for c_n, c_t in t["columns"].items()])
         create_script += ")"
-        conn.execute(create_script)
-        conn.commit()
+        crsr.execute(create_script)
+        crsr.commit()
+
+
+def drop_table(conn, t):
+    crsr = conn.cursor()
+    if crsr.tables(table=t["name"]).fetchone():
+        drop_script = "DROP TABLE " + t["schema"] + "." + t["name"]
+        crsr.execute(drop_script)
+        crsr.commit()
 
 
 def create_db(conn, f):
-    create_table(conn,{"name":"am_meta", "columns":{"met_id":"INT NOT NULL PRIMARY KEY IDENTITY(1,1)",
-                                                    "dt":"DATETIME2 NOT NULL DEFAULT GETDATE()"}})
+    create_table(conn,{"name": "am_meta",
+                       "columns": {"met_id": "INT NOT NULL PRIMARY KEY IDENTITY(1,1)",
+                                   "dt":"DATETIME2 NOT NULL DEFAULT GETDATE()"}})
     for s_o in f["source_objects"]:
         create_table(conn, {"name": s_o["name"] + "_" + s_o["delta_field"],
-                                    "columns":{s_o["delta_field"]: "BIGINT NOT NULL",
-                                               "met_id": "INT NOT NULL PRIMARY KEY",
-                                               "dt": "DATETIME2 NOT NULL DEFAULT GETDATE()"}})
+                            "columns": {s_o["delta_field"]: "BIGINT NOT NULL",
+                                        "met_id": "INT NOT NULL PRIMARY KEY",
+                                        "dt": "DATETIME2 NOT NULL DEFAULT GETDATE()"}})
     for o in f["objects"]:
         if o["type"] == "anchor":
             create_table(conn, {"name": o["name"],
@@ -28,11 +38,11 @@ def create_db(conn, f):
                                     "met_id": "INT NOT NULL"
                                 }})
         elif o["type"] == "attribute":
-            a = [t_o for t_o in f["objects"] and t_o["type"] == "anchor" and t_o["name"] == o["anchor"]][0]
-            create_table(conn, {"name": a["name"]+o["name"],
+            a = [t_o for t_o in f["objects"] if t_o["type"] == "anchor" and t_o["name"] == o["anchor"]][0]
+            create_table(conn, {"name": a["name"] + "_" + o["name"],
                                 "columns":{
                                     a["name"]+"_id": a["column_type"] + " NOT NULL PRIMARY KEY REFERENCES "
-                                    + a["name"] + "(" + a["name"] + "_id) ",
+                                    + conn["schema"] + "." + a["name"] + "(" + a["name"] + "_id) ",
                                     o["column_name"]: o["column_type"] + " NOT NULL",
                                     "met_id": "INT NOT NULL"
                                 }})
@@ -48,13 +58,13 @@ def create_db(conn, f):
                                 }})
 
 
-
 def main():
     with open("anchor_modelling.json") as f_file:
         f = json.load(f_file)
     conn_a = pyodbc.connect(f["connections"]["anchor_connection"]["conn_str"])
-    conn_a.schema = f["connections"]["anchor_connection"]["schema"]
-    create_db(conn_a, f)
+    create_db({"connection": conn_a,
+               "schema": f["connections"]["anchor_connection"]["schema"],
+               "database": f["connections"]["anchor_connection"]["database"]}, f)
 
 
 if __name__ == "__main__":
