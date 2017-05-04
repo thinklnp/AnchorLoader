bc1ccb5342dda9ed42872e6204b3b66b2e164034
Lapa <thinklnp@yandex.ru>
2017-05-05 01:22:53 +0300

    halfway

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def create_db(conn, f):
                        "columns": {"met_id": "INT NOT NULL PRIMARY KEY IDENTITY(1,1)",
                                    "dt":"DATETIME2 NOT NULL DEFAULT GETDATE()"}})
     for s_o in f["source_objects"]:
-        create_table(conn, {"name": s_o["name"] + "_" + s_o["delta_field"],
+        create_table(conn, {"name": "{name}_{df}".format(name=s_o["name"], df=s_o["delta_field"]),
                             "columns": {s_o["delta_field"]: "BIGINT NOT NULL",
                                         "met_id": "INT NOT NULL PRIMARY KEY",
                                         "dt": "DATETIME2 NOT NULL DEFAULT GETDATE()"}})
@@ -49,8 +49,8 @@ def create_db(conn, f):
         elif o["type"] == "attribute":
             a = [t_o for t_o in f["objects"] if t_o["type"] == "anchor" and t_o["name"] == o["anchor"]][0]
             cs = {}
-            cs.update({a["name"]+"_id": a["column_type"] + " NOT NULL PRIMARY KEY REFERENCES "
-                       + conn["schema"] + "." + a["name"] + "(" + a["name"] + "_id) "})
+            cs.update({a["name"]+"_id": "{col_t} NOT NULL PRIMARY KEY REFERENCES {schema}.{an}({an}_id)"
+                                        .format(col_t = a["column_type"], schema=conn["schema"], an=a["name"])})
             if a["knot"]:
                 k = [k_o for k_o in f["objects"] if k_o["type"] == "knot" and k_o["name"] == o["knot"]][0]
                 cs.update({k["name"] + "_id": k["column_type"] + " NOT NULL REFERENCES "
@@ -107,6 +107,34 @@ def create_db(conn, f):
                                 })
 
 
+def get_meta(conn):
+    crsr = conn["connection"].cursor()
+    crsr.execute("INSERT INTO {0}.am_meta(dt) VALUES(GETDATE()) SELECT @@SCOPE_IDENTITY met_id".format(conn["schema"]))
+    met_id = crsr.fetchone().met_id
+    crsr.commit()
+    return met_id
+
+
+def get_rv(s, conn):
+    crsr = conn["connection"].cursor()
+    crsr.execute("SELECT MAX({0}) rv FROM {1}_{0}".format(s["delta_field"],s["name"]))
+    return crsr.fetchone().rv
+
+
+def load_source(s, conn_a, f, rv, met_id, cnt = 10000, par_id = None):
+    columns = set()
+    for o in [fo for fo in f["objects"] if fo["source_name"] == s["name"]]:
+        for c in o["columns"]:
+            columns.add(c["source_column"])
+        columns.add(o["source_column"])
+    curs = pyodbc.connect(f["connections"]["source_connections"][s["connection"]]).cursor()
+    curs.execute(
+        "SELECT TOP {cnt} {clmns} FROM {src} WHERE rv > CAST({m_rv} "
+        .format(cnt=cnt, clmns=", ".join([c for c in columns]), src=s["source_table"], m_rv=rv))
+    rows = curs.fetchall()
+    pass
+
+
 def main():
     with open("anchor_modelling.json") as f_file:
         f = json.load(f_file)
@@ -114,7 +142,9 @@ def main():
     create_db({"connection": conn_a,
                "schema": f["connections"]["anchor_connection"]["schema"],
                "database": f["connections"]["anchor_connection"]["database"]}, f)
-
+    met_id = get_meta(conn_a)
+    for s in f["source_objects"]:
+        load_source(s, conn_a, f, get_rv(s,conn_a), met_id)
 
 if __name__ == "__main__":
     main()
